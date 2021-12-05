9712d1f8e478557a0fa876d4df351f2693d01446
Zoe Rowbotham <zoe2.rowbotham@live.uwe.ac.uk>
2021-12-05 19:21:55 +0000

    Avatar UI

--- Assets/Scripts/UI/Popups/CustomPopups/CustomiseAvatarPopup/AvatarAttribute.cs
+++ Assets/Scripts/UI/Popups/CustomPopups/CustomiseAvatarPopup/AvatarAttribute.cs
@@ -1,17 +1,39 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AvatarAttribute : MonoBehaviour
 {
-    public string CurrentValue => m_attributeValues[m_currentIndex];
+    public string CurrentValue => m_attributePrefix + "_" + m_attributeValues[m_currentIndex];
 
     [SerializeField] private TextMeshProUGUI m_currentValueText = default;
+    [SerializeField] private Button m_negativeButton = default;
+    [SerializeField] private Button m_positiveButton = default;
+
     [SerializeField] private bool m_loopValues = false;
+    [SerializeField] private string m_attributePrefix;
     [SerializeField] private string[] m_attributeValues = { };
 
     private int m_currentIndex = 0;
 
-    public void OnNegativeButton()
+    private void Awake()
+    {
+        UpdateDisplay();
+    }
+
+    private void OnEnable()
+    {
+        m_positiveButton.onClick.AddListener(OnPositiveButton);
+        m_negativeButton.onClick.AddListener(OnNegativeButton);
+    }
+
+    private void OnDisable()
+    {
+        m_positiveButton.onClick.RemoveListener(OnPositiveButton);
+        m_negativeButton.onClick.RemoveListener(OnNegativeButton);
+    }
+
+    private void OnNegativeButton()
     {
         m_currentIndex--;
 
@@ -30,7 +52,7 @@ public class AvatarAttribute : MonoBehaviour
         UpdateDisplay();
     }
 
-    public void OnPositiveButton()
+    private void OnPositiveButton()
     {
         m_currentIndex++;
 
@@ -51,13 +73,11 @@ public class AvatarAttribute : MonoBehaviour
         UpdateDisplay();
     }
 
-    private void Awake()
-    {
-        UpdateDisplay();
-    }
-
     private void UpdateDisplay()
     {
-        m_currentValueText.text = CurrentValue;
+        m_currentValueText.text = m_attributeValues[m_currentIndex];
+
+        m_positiveButton.interactable = m_loopValues || (!m_loopValues && m_currentIndex < m_attributeValues.Length - 1);
+        m_negativeButton.interactable = m_loopValues || (!m_loopValues && m_currentIndex > 0);
     }
 }

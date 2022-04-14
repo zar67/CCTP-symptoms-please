6e79ecfda2a2d00c5806d85c000b884fcf6166cc
Zoe Rowbotham <zoe2.rowbotham@live.uwe.ac.uk>
2022-04-15 00:31:26 +0100

    Panels UI Update

--- Assets/Scripts/UI/Panels/Custom Panels/DayStart/SocialMediaModificationsDisplay.cs
+++ Assets/Scripts/UI/Panels/Custom Panels/DayStart/SocialMediaModificationsDisplay.cs
@@ -1,3 +1,4 @@
+using SymptomsPlease.UI.Popups;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,22 @@ public class SocialMediaModificationsDisplay : MonoBehaviour
 
     [SerializeField] private Transform m_scrollViewContent = default;
     [SerializeField] private ModificationDisplay m_displayPrefab = default;
+
+    [Header("Popup References")]
+    [SerializeField] private PopupData m_popupsData = default;
+    [SerializeField] private string m_stiInformationPopupName = "";
+
+    [Header("STI Information References")]
+    [SerializeField] private AllAfflictionDatas m_afflictionDatas = default;
+    [SerializeField] private STIInfoButton m_infoButtonPrefab = default;
+
+    private STIInformationPopup m_stiInformationPopup = default;
+
+    private void Start()
+    {
+        m_stiInformationPopup = (STIInformationPopup)m_popupsData.GetPopup(m_stiInformationPopupName);
+    }
+
 
     private void OnEnable()
     {
@@ -25,6 +42,15 @@ public class SocialMediaModificationsDisplay : MonoBehaviour
         {
             ModificationDisplay display = Instantiate(m_displayPrefab, m_scrollViewContent);
             display.SetText(topic.Value.Description);
+
+            foreach (AfflictionData affliction in m_afflictionDatas.GetAfflictionsWithTopic(topic.Key))
+            {
+                STIInfoButton infoButton = Instantiate(m_infoButtonPrefab, display.InfoButtonHolder);
+                infoButton.SelectButton.onClick.AddListener(() => m_stiInformationPopup.UpdateDisplay(affliction));
+                infoButton.SetNameText(affliction.DisplayName);
+            }
+
+            display.UpdateLayout();
         }
     }
 }
--- Assets/Scripts/UI/Panels/Custom Panels/Training/TopicRanking.cs
+++ Assets/Scripts/UI/Panels/Custom Panels/Training/TopicRanking.cs
@@ -3,12 +3,10 @@ using UnityEngine;
 
 public class TopicRanking : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI m_topicTitleText = default;
     [SerializeField] private TextMeshProUGUI m_topicScoreText = default;
 
     public void SetData(string title, int score)
     {
-        m_topicTitleText.text = title + ":";
-        m_topicScoreText.text = score.ToString();
+        m_topicScoreText.text = title + ": " + score.ToString();
     }
 }
